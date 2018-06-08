8add7c58bcd1508f4e579baa24bdcfb20aec0364
garfield <garfield@mail.startek-eng.com>
2018-06-08 15:52:44 +0800

    8. Event Loop Ticks

--- loop.js
+++ loop.js
@@ -16,7 +16,20 @@ function shouldContinue() {
 
 // Entire body executes in one 'tick'
 while(shouldContinue()) {
+  // 1) Node looks at pendingTimers and sees if any functions
+  // are ready to be called
 
+  // 2) Node looks at pendingOSTasks and pendingOperations
+  // and calls relevant callbacks
+
+  // 3) Pause execution. Continue when...
+  //  - a new pendingOSTask is done
+  //  - a new pendingOperation is done
+  //  - a timer is about to complete
+
+  // 4) Look at pendingTimers. Call any setImmediate
+
+  // 5) Handle any 'close' events
 }
 
 
