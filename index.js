29994e3ce5d3fb2a84c0b575304c72c7875b5611
garfield <cmgarfield@gmail.com>
2018-06-09 22:56:06 +0800

    28. Benchmark Refactor

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+process.env.UV_THREADPOOL_SIZE = 1;
 const cluster = require('cluster');
 
 // Is the file being executed in master mode?
@@ -12,16 +13,13 @@ if (cluster.isMaster) {
   // I'm a child, I'm going to act like a server
   // and do nothing else
   const express = require('express');
+  const crypto = require('crypto');
   const app = express();
 
-  function doWork(duration) {
-    const start = Date.now();
-    while (Date.now() - start < duration) {}
-  }
-
   app.get('/', (req, res) => {
-    doWork(5000);
-    res.send('Hi there');
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+      res.send('Hi there');
+    });
   });
 
   app.get('/fast', (req, res) => {
