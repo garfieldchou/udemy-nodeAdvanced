251f5aac533f59f66b74668787676650e2cae33f
garfield <cmgarfield@gmail.com>
2018-06-10 16:17:01 +0800

    34. Benchmarking Workers

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ app.get('/', (req, res) => {
     };
   });
 
-  worker.onmessage = function(myCounter) {
-    console.log(myCounter);
+  worker.onmessage = function(message) {
+    console.log(message.data);
+    res.send('' + message.data);
   };
 
   worker.postMessage();
